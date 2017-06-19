5d3b9176d745e2833bc085a662169d1eb2470226
Dmitry Palamarchuk <moonbrv@gmail.com>
2017-06-19 22:47:56 +0300

    add animation to the main screen

--- src/js/nav-bar.js
+++ src/js/nav-bar.js
@@ -3,9 +3,10 @@ import { TimelineLite } from 'gsap'
 export default class NavBarAnimation {
   constructor () {
     this.logo = document.querySelector('.main-navbar__logo')
+    this.titleLineClass = 'main-title__line'
 
     this.addSVGmotion = this.addSVGmotion.bind(this)
-
+    this.splitTextToLines('.main-title')
     this.addListeners()
   }
 
@@ -18,9 +19,14 @@ export default class NavBarAnimation {
     const lang = document.querySelector('.lang')
     const menuIconItems = Array.from(document.querySelectorAll('.menu-icon__item'))
     this.navBarAnimation = new TimelineLite()
-    this.navBarAnimation.delay(0.5)
+    this.navBarAnimation.delay(1)
     const straggerPartOfMenuIcon = menuIconItems.slice(0, 2).reverse()
     const menuBtnText = document.querySelector('.menu-btn__text')
+    const playIcon = document.querySelector('.header-play-icon')
+    const titleLines = Array.from(document.querySelectorAll('.main-title__line'))
+    const playIconLabel = document.querySelector('.header-showreel')
+    const scrollIcon = document.querySelector('.header-scroll-icon')
+
     this.navBarAnimation
     .from(logoIcon[0], time, {opacity: 0, y: '+=20'}, 'first')
     .from(logoIcon[1], time, {opacity: 0, x: '+=20'}, 'second')
@@ -29,15 +35,53 @@ export default class NavBarAnimation {
     .from(lang, time, {opacity: 0, y: '+=20'}, 'second')
     .from(menuIconItems[2], time, {opacity: 0, y: '+=20'}, 'first')
     .staggerFrom(straggerPartOfMenuIcon, (time / 2), {opacity: 0, x: '+=5'}, (time / 2), 'second')
+    .staggerFrom(titleLines, 0.5, {opacity: 0, y: '+=20'}, 0.3, 'third')
+    .from(playIcon, 0.7, {rotation: 180, scale: 0.5, opacity: 0}, 'fourth')
+    .from(playIconLabel, (time / 2), {opacity: 0, y: '-=20'}, 'fifth')
+    .from(scrollIcon, (time / 2), {opacity: 0}, 'fifth')
     // this.navBarAnimation.pause()
     /* End onSiteLoad Over Effect */
   }
 
+  splitTextToLines (querySelector) {
+    const paragraph = document.querySelector(querySelector)
+    let wrapStrings = []
+
+    let text = paragraph.textContent
+    text = text.trim()
+    const words = text.split(' ')
+
+    paragraph.textContent = words[0]
+    let height = paragraph.offsetHeight
+
+    let k = 0
+    wrapStrings[k] = []
+    wrapStrings[k].push(words[0])
+
+    for (let i = 1; i < words.length; i++) {
+      paragraph.textContent = `${paragraph.textContent} ${words[i]}`
+      if (paragraph.offsetHeight > height) {
+        height = paragraph.offsetHeight
+        k += 1
+        wrapStrings[k] = []
+        wrapStrings[k].push(words[i])
+      } else {
+        wrapStrings[k].push(words[i])
+      }
+    }
+    wrapStrings = wrapStrings.map(arr => {
+      return arr.join(' ')
+    })
+    paragraph.textContent = ''
+    wrapStrings.forEach(str => {
+      const span = document.createElement('span')
+      span.classList.add(this.titleLineClass)
+      span.textContent = str
+      paragraph.appendChild(span)
+    })
+  }
+
   addListeners () {
     this.logo.addEventListener('load', this.addSVGmotion)
-    const text = document.querySelector('.main-title').textContent
-    console.log(text.trim().match(/\r?\n/g))
-    const splitOption = text.match(/\r?\n/g)[0]
-    console.log(text.lastIndexOf(splitOption))
   }
 }
